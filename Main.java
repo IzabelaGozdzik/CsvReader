6232614ace6429c9f479fb64067cd7a2d1db01cb
igozd <i.gozdzik@wp.pl>
2024-07-25 16:01:57 +0200

    read csv file

--- Main.java
+++ Main.java
@@ -1,8 +1,30 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        System.out.println("hello world");
+        String line = "";
+        String splitBy = ",";
+        List<String[]> student = new ArrayList<>();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("uczniowie.csv"));
+            while ((line = br.readLine()) != null)   //returns a Boolean value
+            {
+                student.add(line.split(splitBy));    // use comma as separator
+            }
+        } catch (Exception e) {
+            System.out.println("Daj pan plik poprawny");
+        } finally {
+            System.out.println("wykona sie zawsze");
+        }
     }
 }
